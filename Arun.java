4221f5dc25227edb6745a410bdd8ad1a7a4313cf
ArunsivaK <113993754+ArunsivaK@users.noreply.github.com>
2022-11-17 16:24:56 +0530

    Update Arun.java

--- Arun.java
+++ Arun.java
@@ -1,5 +1,6 @@
 public class Arun {
     public static void main(String[] args) {
-        System.out.println("Hello world...");
+        int a=10;
+        System.out.println("Hello world..."+a);
     }
 }
