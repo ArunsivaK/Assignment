fa6316aee99b0840edc03e99c149930fb50d9d12
ThamizhArasanS <thamizharasans@nmsworks.co.in>
2022-11-18 10:12:48 +0530

    added sout

--- Arun.java
+++ Arun.java
@@ -1,6 +1,7 @@
 public class Arun {
     public static void main(String[] args) {
         int a=10;
+        System.out.println("Changed by tamizh");
         System.out.println("Hello world..."+a);
     }
 }
