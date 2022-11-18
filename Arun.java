60aea358eacbc8826c46b65f4157c6fb2d597d18
ArunsivaK <arunsivak@nmsworks.co.in>
2022-11-18 10:16:50 +0530

    variable d is added

--- Arun.java
+++ Arun.java
@@ -2,6 +2,7 @@ public class Arun {
     public static void main(String[] args) {
         int a=10;
         int c=30;
+        int d = 66;
         System.out.println("Hello world..."+a);
         System.out.println("hi");
     }
